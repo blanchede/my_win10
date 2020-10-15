0886cd40fd926fbfc7ec1bb313b2af2904a3b6cb
blanche-win10 <123456win@qq.com>
2020-10-15 21:05:37 +0800

    dev test 01

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("good");
         System.out.println("error3");
+        System.out.println("dev test");
     }
 }
